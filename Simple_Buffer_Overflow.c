9857db380b801b523e40c2c541f7f05247ba7f3a
saadfahmitest <s.fahmi2618@uca.ac.ma>
2025-01-07 23:07:01 +0100

    demo

---
+++ Simple_Buffer_Overflow.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void func(int key)
+{
+            char overflowme[32];
+            printf("overflow me:  ");
+            gets(overflowme);
+            if(key == 0xcafebabe)
+            {
+                    system("/bin/sh");
+
+            }
+                else
+            {
+                printf("nah..\n");
+
+
+            }
+
+}
+int main(int argc, char *argv[])
+{
+        func(0xdeadbeef);
+        return(0);
+        
+
+}
